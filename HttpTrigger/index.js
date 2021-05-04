e368657e849083e9f26d7198eae2084e067e59b7
Diego Cravo Teixeira <cravo.diego@gmail.com>
2021-05-04 14:16:31 -0300

    first commit

---
+++ HttpTrigger/index.js
@@ -0,0 +1,15 @@
+const createMongoClient = require("../shared/mongoClient");
+
+module.exports = async function (context, req) {
+  const { client: MongoClient, closeConnectionFn } = await createMongoClient();
+  const Products = await MongoClient.collection("products");
+  const res = await Products.find();
+  const body = await res.toArray();
+
+  closeConnectionFn();
+
+  context.res = {
+    status: 200,
+    body,
+  };
+};
